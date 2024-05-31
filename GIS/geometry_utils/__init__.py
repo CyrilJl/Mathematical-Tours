bd8fcfecf69a7fe109b3293b7588da08efb3a817
CyrilJl <joly.cyril7@gmail.com>
2024-05-31 17:07:43 +0200

    up

--- GIS/geometry_utils/__init__.py
+++ GIS/geometry_utils/__init__.py
@@ -1,3 +1,3 @@
-from .core import covering_mesh, generator_geopandas
+from .core import covering_mesh, generator_geopandas, proj, get_coordinates
 
-__all__ = ['covering_mesh', 'generator_geopandas']
+__all__ = ['covering_mesh', 'generator_geopandas', 'proj', 'get_coordinates']
